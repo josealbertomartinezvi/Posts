874870c6677d6ae4b9a6abc0b5effb4d74ecd5af
Jose Martinez <josealbertomartinez.vi@gmail.com>
2020-05-22 20:32:32 -0500

    modelos configurados

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content', 'post_id', 'user_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    public function post(){
+        return $this->belongsTo('App\Models\Post', 'post_id');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'user_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    public function comments(){
+        return $this->hasMany('App\Models\Comment', 'post_id');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
